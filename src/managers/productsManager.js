e08e15a268aa60b9f4a988ae4d34543f0630aa55
Lolitoox <guidobasano@hotmail.com>
2024-05-04 10:15:58 -0300

    Correccion

--- src/managers/productsManager.js
+++ src/managers/productsManager.js
@@ -1,60 +1,90 @@
-const fs = require('fs/promises');
+import fs from "fs";
 
 class ProductManager {
-    constructor(path) {
-        this.path = path;
-        this.products = [];
-        this.id = 0;
-        this.status = true;
+  constructor(path) {
+    this.path = path;
+    this.products = [];
+    this.id = 0;
+    this.status = true;
+  }
+  //Función para iniciar el pruductData
+  async initialize() {
+    try {
+      const productsData = await fs.readFile(this.path, "utf-8");
+      this.products = JSON.parse(productsData);
+      this.id =
+        this.products.length > 0
+          ? this.products[this.products.length - 1].id
+          : 0;
+    } catch (error) {
+      console.error("Error al inicializar ProductManager:", error);
     }
-//Función para iniciar el pruductData
-    async initialize() {
-        try {
-            const productsData = await fs.readFile(this.path, 'utf-8');
-            this.products = JSON.parse(productsData);
-            this.id = this.products.length > 0 ? this.products[this.products.length - 1].id : 0;
-        } catch (error) {
-            console.error('Error al inicializar ProductManager:', error);
-        }
+  }
+  //Función para agregar productos
+  async addProduct(product) {
+    try {
+      product.id = ++this.id;
+      this.products.push(product);
+      await this.saveProductsToFile();
+    } catch (error) {
+      console.error("Error al agregar el producto:", error);
     }
-//Función para agregar productos
-    addProduct(product) {
-        product.id = ++this.id;
-        this.products.push(product);
-        this.saveProductsToFile();
+  }
+  //Función para pedir productos con un limite
+  async getProducts(limit) {
+    try {
+      const productsData = await fs.readFile(this.path, "utf-8");
+      const products = JSON.parse(productsData);
+      const limitedProducts = limit
+        ? products.slice(0, parseInt(limit))
+        : products;
+      return limitedProducts;
+    } catch (error) {
+      console.error("Error al obtener los productos:", error);
+      return [];
     }
-//Función para pedir productos con un limite
-    getProducts(limit) {
-        const limitedProducts = limit ? this.products.slice(0, parseInt(limit)) : this.products;
-        return limitedProducts;
+  }
+  //Función para buscar productos por id
+  async getProductById(id) {
+    try {
+      const productsData = await fs.readFile(this.path, "utf-8");
+      const products = JSON.parse(productsData);
+      return products.find((product) => product.id === id);
+    } catch (error) {
+      console.error("Error al obtener el producto por ID:", error);
+      return null;
     }
-//Función para buscar productos por id
-    getProductById(id) {
-        return this.products.find(product => product.id === id);
+  }
+  //Función para actualizar productos
+  async updateProduct(id, updatedProduct) {
+    try {
+      const index = this.products.findIndex((product) => product.id === id);
+      if (index !== -1) {
+        this.products[index] = { ...updatedProduct, id };
+        await this.saveProductsToFile();
+      }
+    } catch (error) {
+      console.error("Error al actualizar el producto:", error);
     }
-//Función para actualizar productos
-    updateProduct(id, updatedProduct) {
-        this.products = this.products.map(product => {
-            if (product.id === id) {
-                return { ...product, ...updatedProduct, id };
-            }
-            return product;
-        });
-        this.saveProductsToFile();
+  }
+  //Función para borrar un producto
+  async deleteProduct(id) {
+    try {
+      this.products = this.products.filter((product) => product.id !== id);
+      await this.saveProductsToFile();
+    } catch (error) {
+      console.error("Error al eliminar el producto:", error);
     }
-//Función para borrar un producto
-    deleteProduct(id) {
-        this.products = this.products.filter(product => product.id !== id);
-        this.saveProductsToFile();
+  }
+
+  //Función para guardar los productos
+  async saveProductsToFile() {
+    try {
+      await fs.writeFile(this.path, JSON.stringify(this.products));
+    } catch (error) {
+      console.error("Error al guardar los productos en el archivo:", error);
     }
-//Función para guardar los productos
-    async saveProductsToFile() {
-        try {
-            await fs.writeFile(this.path, JSON.stringify(this.products));
-        } catch (error) {
-            console.error('Error al guardar los productos en el archivo:', error);
-        }
-    }
+  }
 }
 
 module.exports = ProductManager;
